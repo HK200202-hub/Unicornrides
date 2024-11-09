f51172053b7394de4f0dad360cc7b60aba2acfa3
HK200202-hub <hari200202@gmail.com>
2024-11-09 01:47:59 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region: ' us-east-2' // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://sw2ks7hac8.execute-api.us-east-2.amazonaws.com/devop' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
