4d97230b21f888c021ce5c76f788740caaac6378
HK200202-hub <hari200202@gmail.com>
2024-11-15 14:14:57 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-2_ev27qDsGX', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '3h0jj7bap7iaufsapgar8eocdr', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: '*', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '*', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: ' us-east-2' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://sw2ks7hac8.execute-api.us-east-2.amazonaws.com/devop' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: '*' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
